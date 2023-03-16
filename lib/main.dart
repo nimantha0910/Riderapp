17e9406643fb3f6b8c14245c7a3f0140682ae29e
nimantha0910 <nimanthamanage1111@gmail.com>
2023-03-16 19:51:43 +0530

    initialize project

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+import 'package:riderapp/AllScreens/loginScreen.dart';
+import 'package:riderapp/AllScreens/registrationScreen.dart';
+import 'AllScreens/mainscreen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+DatabaseReference userRef = FirebaseDatabase.instance.ref().child("users");
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: ' Parking App ',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          fontFamily: " Signatra ",
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        initialRoute: loginScreen.idScreen,
+        routes: {
+          registrationScreen.idScreen: (context) => registrationScreen(),
+          loginScreen.idScreen: (context) => loginScreen(),
+          MainScreen.idScreen: (context) => MainScreen(),
+        });
+  }
+}
